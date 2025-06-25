24aaf072eeb0caa59b4761e2027742d7eed0a013
mciiee <mciiee@klasna.lol>
2025-06-25 17:24:13 +0500

    dev: added header guard for tokenizer.h

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,6 +1,12 @@
 #pragma once
+
+#ifndef _TOKENIZER_H
+#define _TOKENIZER_H
+#endif
+
 #ifndef _TOKEN_DYANMIC_ARRAY_H
 #include "token_dynamic_array.h"
 #endif
+
 #include <stdio.h>
 TokenDynamicArray *tokenize(FILE *stream);
